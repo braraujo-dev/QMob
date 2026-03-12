2c09f2f0477a392fd31147318fd96415f3c90d72
FMoreiraSouza <fmoreirasouza701@gmail.com>
2026-03-12 03:16:43 -0300

    Criação de tela principal para administrador de sindicato

---
+++ lib/routes/app_routes_manager.dart
@@ -0,0 +1,17 @@
+﻿import 'package:alternative/features/home/presentation/pages/admin_home_page.dart';
+import 'package:alternative/features/home/presentation/pages/driver_register_page.dart';
+import 'package:flutter/material.dart';
+import '../../features/auth/presentation/pages/auth_page.dart';
+
+class AppRoutes {
+  static const String initial = '/';
+  static const String auth = '/auth';
+  static const String adminHome = '/adminHome';
+  static const String driverRegister = '/driverRegister';
+
+  static Map<String, WidgetBuilder> get routes => {
+    auth: (context) => const AuthPage(),
+    adminHome: (context) => const AdminHomePage(),
+    driverRegister: (context) => const DriverRegisterPage(),
+  };
+}
